dd43369e11d808e619e1e66c6ec20a15474dcaf0
rgf1987 <rubenglez1987@gmail.com>
2023-07-05 21:18:29 +0200

    Initial commit

---
+++ src/main/java/crossfitmieres/workouts/WorkoutsApplication.java
@@ -0,0 +1,13 @@
+package crossfitmieres.workouts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkoutsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkoutsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/crossfitmieres/workouts/WorkoutsApplicationTests.java
@@ -0,0 +1,13 @@
+package crossfitmieres.workouts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkoutsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
